389206e8d6dcf039c2d2fdc787f4be8803142687
shrushti <shrushti.y.kavatekar@gmail.com>
2026-02-05 14:30:52 +0530

    basic java files

---
+++ AccountHolder.java
@@ -0,0 +1,17 @@
+class AccountHolder{
+
+String accountHolderName = "Srushti";
+
+      
+
+public static void main(String[]accounts){
+
+String accountHolderName = "Dev";
+
+ System.out.println("Srushti");
+ int accountNumber = 741852963 ;
+ 
+ System.out.println(accountNumber);
+}
+
+}
---
+++ Mountain.java
@@ -0,0 +1,9 @@
+class Mountainc {
+public static void main(String[] mountain) {
+    String mountainName = " Mt Fuji ";
+    String climateCondtion = "cold";
+
+    System.out.println("The Name of Mountain is :" + mountainName);
+    System.out.println("The Mountain Condition is " + climateCondtion);
+}
+}
