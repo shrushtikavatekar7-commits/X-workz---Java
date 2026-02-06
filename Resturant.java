a5e21bc3aecb785b2fa587dd0268bc78823c3659
shrushti <shrushti.y.kavatekar@gmail.com>
2026-02-06 12:39:04 +0530

    adding array related files

---
+++ Resturant.java
@@ -0,0 +1,32 @@
+class Resturant {
+
+public static void main (String[] res){
+
+ String name = "Udupi Resturant";
+ String owner = "Raj";
+ String address = "Majestic";
+ 
+ String branches [] = { "Majestic","Jp Nagar "," RR nagar ","Rajrajinagar "};
+ String managers [] = {"Ammu","Nandu","Chutti","Tanu"};
+ 
+ System.out.println("The Resturant info is :");
+ System.out.println("The name of Resturantis " + name);
+ System.out.println("The owner name is "+ owner);
+ System.out.println(" The address of Resturant is " + address);
+ 
+ System.out.println("The no of Branches are :" + branches.length);
+ System.out.println("List of Branches available:");
+ 
+ for(String branch: branches){
+ System.out.println(branch);
+ 
+       }
+    }
+ }
+ 
+ 
+ 
+
+ 
+ 
+
